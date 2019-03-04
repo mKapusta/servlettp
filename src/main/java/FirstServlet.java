045c931a3bf83bb2e5e5dab93a29d75da7438250
MaxKap <maxime.kapusta@symbol-it.fr>
2019-03-04 14:20:29 +0100

    init

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello.do")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp)
+            throws ServletException, IOException {
+      resp.getWriter().write("<html><head></head><body>toto</body>");
+
+
+      String parameter= req.getParameter("produit");
+
+
+
+
+
+
+
+      System.out.println(parameter);
+    }
+}
